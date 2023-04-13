277837974f4f7086b4ee6bf3b5d39493295b730d
Robin <robin.noormets@voco.ee>
2023-04-13 09:23:18 +0300

    Ülesanne 3 - ExpenseItem ümber kujundamine olemasoleva komponendi abil

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props){
-    console.log(props)
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.expenseData.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.expenseData.price}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
