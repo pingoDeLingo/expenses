c1a158585d1443b1a2c9b6c63a492e8b9a73c510
Robin <robin.noormets@voco.ee>
2023-04-17 12:49:40 +0300

    8. Sündmuste töötlus komponentides

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,18 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from './Card'
 
 function ExpenseItem(props){
+    const [title, setTitle] = useState(props.expenseData.expenseData.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    }
+
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.expenseData.expenseData.date}></ExpenseDate>
@@ -10,6 +20,7 @@ function ExpenseItem(props){
                 <h2>{props.expenseData.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.expenseData.price}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
